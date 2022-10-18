ce9c03a3f000c2f10453c889af3c97bbb247c892
Marcos Miguel dos Santos <114240224+setfocus2022@users.noreply.github.com>
2022-10-18 13:43:24 -0300

    150

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -13,7 +13,7 @@ export const Footer = ({ children, link = "/" }) => {
                 src="/icons/logo_transparente.png"
                 alt="Logo"
                 width="300"
-                height="110"
+                height="150"
               />
             </a>
           </Link>
