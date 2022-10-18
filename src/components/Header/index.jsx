8ba85175c85261ef499aff941f74fcd38b70e4a0
Marcos Miguel dos Santos <114240224+setfocus2022@users.noreply.github.com>
2022-10-18 11:37:48 -0300

    140

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -40,7 +40,7 @@ export const Header = () => {
               <Image
                 src="/icons/logo_transparente.png"
                 alt="Eletro Santa Clara"
-                width="180"
+                width="140"
                 height="64"
               />
             </a>
