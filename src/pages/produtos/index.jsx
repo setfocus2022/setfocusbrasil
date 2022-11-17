9cb5b234fa0d3906d03ece3612749e5971f8a48c
Marcos Miguel dos Santos <114240224+setfocus2022@users.noreply.github.com>
2022-11-17 12:35:43 -0300

    strong2

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -44,7 +44,7 @@ export default function Tratamentos() {
           <h3>Pizzaria e Lanchonetes - Delivery em Geral</h3>
           <div className={styles.description}>
             <p>
-           <Sttong2>Site Disk Entrega para Pizzaria desenvolvido em ReactJS - Brinde hospedagem</Sttong2> 
+           <Strong2>Site Disk Entrega para Pizzaria desenvolvido em ReactJS - Brinde hospedagem</Strong2> 
            </p>
 
            <p>
