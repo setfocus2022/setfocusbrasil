17383103d4891ccee1f85a8642ecbcc0efb45426
Marcos Miguel dos Santos <114240224+setfocus2022@users.noreply.github.com>
2022-11-17 16:09:39 -0300

    bucha

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -94,22 +94,33 @@ export default function Tratamentos() {
 
         <div className={styles.treatmentsItem} data-aos="fade-up">
           <MyImage
-            src="/images/prod_3.png"
-            alt="Toda linha de conduletes e acessórios."
+            src="/images/prod_2.png"
+            alt="Materiais Elétricos"
             width="350"
             height="279"
-            left
           />
-          <h3>Conduletes</h3>
+          <h3>Materiais Elétricos</h3>
           <div className={styles.description}>
-            <p>
-            Se é de conexão e condução que voê precisa, nós dispomos de toda linha de conduletes e acessórios.
+          <p>
+            Site para Loja de Material Elétrico com foco no fortalecmento da marca e aumento da area de atuação de vendas.
+        
+          <p>Desenvolvido em ReactJs de alto desempenho, rapidez e SEO otimizado para o Google, além de estar integrado com o Google Analytcs e Ads para um melhor acompanhamento do desempenho. </p>
+           <p>Valor já inclui o registro do domínio por 01 ano. Brinde hospedagem grátis por 01 ano. 
+           Não perca tempo e tenha uma site de alta tecnologia por apenas R$ 2.500,00 já com o seu dominio .com.br e alavanque suas Vendas.</p>
+           <p>Veja modelo  :</p>   
+           <Link href="https://www.eletrosantaclara.com.br">
+                  <a target="_blank">
+                  Eletro Santa Clara
+                  </a>
+                </Link>
+           
             </p>
+           
           </div>
         </div>
 
 
-        <div className={styles.treatmentsItem} data-aos="fade-up">
+          <div className={styles.treatmentsItem} data-aos="fade-up">
           <MyImage
             src="/images/prod_2.png"
             alt="Materiais Elétricos"
@@ -137,25 +148,7 @@ export default function Tratamentos() {
         </div>
 
 
-        <div className={styles.treatmentsItem} data-aos="fade-up">
-          <MyImage
-            src="/images/prod_3.png"
-            alt="Toda linha de conduletes e acessórios."
-            width="350"
-            height="279"
-            left
-          />
-          <h3>Conduletes</h3>
-          <div className={styles.description}>
-            <p>
-            Se é de conexão e condução que voê precisa, nós dispomos de toda linha de conduletes e acessórios.
-            </p>
-          </div>
-        </div>
-
-
-
-        <div className={styles.treatmentsItem} data-aos="fade-up">
+         <div className={styles.treatmentsItem} data-aos="fade-up">
           <MyImage
             src="/images/prod_2.png"
             alt="Materiais Elétricos"
@@ -183,200 +176,7 @@ export default function Tratamentos() {
         </div>
 
 
-        <div className={styles.treatmentsItem} data-aos="fade-up">
-          <MyImage
-            src="/images/prod_3.png"
-            alt="Toda linha de conduletes e acessórios."
-            width="350"
-            height="279"
-            left
-          />
-          <h3>Conduletes</h3>
-          <div className={styles.description}>
-            <p>
-            Se é de conexão e condução que voê precisa, nós dispomos de toda linha de conduletes e acessórios.
-            </p>
-          </div>
-        </div>
-
-
-
-        <div className={styles.treatmentsItem} data-aos="fade-up">
-          <MyImage
-            src="/images/prod_2.png"
-            alt="Materiais Elétricos"
-            width="350"
-            height="279"
-          />
-          <h3>Materiais Elétricos</h3>
-          <div className={styles.description}>
-          <p>
-            Site para Loja de Material Elétrico com foco no fortalecmento da marca e aumento da area de atuação de vendas.
-        
-          <p>Desenvolvido em ReactJs de alto desempenho, rapidez e SEO otimizado para o Google, além de estar integrado com o Google Analytcs e Ads para um melhor acompanhamento do desempenho. </p>
-           <p>Valor já inclui o registro do domínio por 01 ano. Brinde hospedagem grátis por 01 ano. 
-           Não perca tempo e tenha uma site de alta tecnologia por apenas R$ 2.500,00 já com o seu dominio .com.br e alavanque suas Vendas.</p>
-           <p>Veja modelo  :</p>   
-           <Link href="https://www.eletrosantaclara.com.br">
-                  <a target="_blank">
-                  Eletro Santa Clara
-                  </a>
-                </Link>
-           
-            </p>
-           
-          </div>
-        </div>
-
-
-        <div className={styles.treatmentsItem} data-aos="fade-up">
-          <MyImage
-            src="/images/prod_3.png"
-            alt="Toda linha de conduletes e acessórios."
-            width="350"
-            height="279"
-            left
-          />
-          <h3>Conduletes</h3>
-          <div className={styles.description}>
-            <p>
-            Se é de conexão e condução que voê precisa, nós dispomos de toda linha de conduletes e acessórios.
-            </p>
-          </div>
-        </div>
-
-        <div className={styles.treatmentsItem} data-aos="fade-up">
-          <MyImage
-            src="/images/prod_4.png"
-            alt="disjuntores"
-            width="350"
-            height="279"
-          />
-          <h3>Disjuntores</h3>
-          <div className={styles.description}>
-            <p>
-            A palavra chave desde a linha residêncial até à Industra 4.0 é <strong2>Segurança</strong2>. Por isso dispomos de toda linha de disjuntores, disjuntor-motor, contatores e relês das mais conceituadas marcas do mercado.
-            </p>
-            <p>
-              
-            </p>
-          </div>
-        </div>        
-
-        
-
-        <div className={styles.treatmentsItem} data-aos="fade-up">
-          <MyImage
-            src="/images/prod_5.png"
-            alt="ELETROCALHAS E PERFILADOS"
-            width="350"
-            height="279"
-            left
-          />
-          <h3>Eletrocalhas e Perfilados</h3>
-          <div className={styles.description}>
-            <p>
-            Ótimas soluções para ambientes que precisam de uma solução pratica e economica para a condução de fios e cabos. Por isso temos toda linha de eletrocalhas, perfilados e acessórios.
-            </p>
-          </div>
-        </div>
-
-        <div className={styles.treatmentsItem} data-aos="fade-up">
-          <MyImage
-            src="/images/prod_12.png"
-            alt="MÉDIA E ALTA TENSÃO"
-            width="350"
-            height="279"
-            left
-          />
-          <h3>Alta Tensão</h3>
-          <div className={styles.description}>
-            <p>
-            Falou de força bruta conte com a gente. Média e Alta Tensão é o nosso DNA. Transformadores, cabos multiplexados, chaves e acessórios.
-            </p>
-          </div>
-        </div>
-
-        <div className={styles.treatmentsItem} data-aos="fade-up">
-          <MyImage
-            src="/images/prod_6.png"
-            alt="Diversos modelos de interruptores e tomadas."
-            width="350"
-            height="279"
-            left
-          />
-          <h3>Interruptores e Tomadas</h3>
-          <div className={styles.description}>
-            <p>
-            Pensou em um ótimo acabamento com qualidade e estilo você encontra aqui! Diversos modelos de interruptores e tomadas.
-            </p>
-          </div>
-        </div>
-
-        <div className={styles.treatmentsItem} data-aos="fade-up">
-          <MyImage
-            src="/images/prod_7.png"
-            alt="Toda linha de lâmpadas e acessórios em LED e demais."
-            width="350"
-            height="279"
-            left
-          />
-          <h3>Lâmpadas</h3>
-          <div className={styles.description}>
-            <p>
-            Seja qual for o ambiente, resdêncial ou industrial uma ótima e econômica iluminação faz toda a diferença. Por isso temos toda linha de lâmpadas, reatores e acessórios que atendem do residêncial ao industrial.
-            </p>
-          </div>
-        </div>
-
-        <div className={styles.treatmentsItem} data-aos="fade-up">
-          <MyImage
-            src="/images/prod_8.png"
-            alt="Toda linha de luminárias e plafons."
-            width="350"
-            height="279"
-            left
-          />
-          <h3>Luminárias</h3>
-          <div className={styles.description}>
-            <p>
-            Pra quem não abre mão da elegância e além de tudo ter segurança e economia em sua residência. Toda linha de luminárias e plafons a pronta entrega.
-            </p>
-          </div>
-        </div>
-
-        <div className={styles.treatmentsItem} data-aos="fade-up">
-          <MyImage
-            src="/images/prod_9.png"
-            alt="Grande variedade de lustres e pendentes."
-            width="350"
-            height="279"
-            left
-          />
-          <h3>Lustres</h3>
-          <div className={styles.description}>
-            <p>
-            Quer dar um toque de elegância e beleza ao seu lar? Conte com a grande variedade de lustres e pendentes.
-            </p>
-          </div>
-        </div>
-
-        <div className={styles.treatmentsItem} data-aos="fade-up">
-          <MyImage
-            src="/images/prod_10.png"
-            alt="Painéis de montagem e acessórios."
-            width="350"
-            height="279"
-            left
-          />
-          <h3>Painéis de montagem</h3>
-          <div className={styles.description}>
-            <p>
-            Painéis de montagem e acessórios.
-            </p>
-          </div>
-        </div>
-
+  
         {/* <div className={styles.treatmentsItem} data-aos="fade-up">
           <MyImage
             src="/images/"
