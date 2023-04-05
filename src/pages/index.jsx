7bbb4b1b8127a4192c483bf665e51bcbae684469
setfocusbrasil <119501939+setfocusbrasil@users.noreply.github.com>
2023-04-05 20:58:20 -0300

    faça

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -58,7 +58,7 @@ export default function Home() {
         <div className={`mainContainer ${styles.heroWrap}`}>
           <section className={styles.leftContent}>
             <h1 className="title" data-aos="fade-up">
-            Fala <strong>Acontecer! </strong>
+            Faça <strong>Acontecer! </strong>
             </h1>
 
           
